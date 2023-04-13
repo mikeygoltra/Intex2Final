e8addb275f39cf321c6c324f072020c51657656e
Spenser Elder <Spenser Elder@LAPTOP-57EK019F>
2023-04-12 18:31:58 -0600

    Add project files.

---
+++ Intex2Final/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Intex2Final.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex2Final.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private intex2dbContext context;
+
+        public HomeController(ILogger<HomeController> logger, intex2dbContext temp)
+        {
+            _logger = logger;
+            context = temp;
+        }
+
+        public IActionResult Test()
+        {
+            List<Burialmain> bury = context.Burialmain.ToList();
+            ViewBag.Burialmain = bury;
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
