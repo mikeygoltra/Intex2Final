7e3abbb14b412626e639ed7479f3e51e2a77ebc1
Josh Bello <superturboturtle@gmail.com>
2023-04-13 00:35:26 -0600

    Joshy Bois committage

--- Intex2Final/Models/Burialmain.cs
+++ Intex2Final/Models/Burialmain.cs
@@ -12,18 +12,18 @@ namespace Intex2Final.Models
         public long Id { get; set; }
         public string Squarenorthsouth { get; set; }
         public string Headdirection { get; set; }
-        public string Sex { get; set; }
-        public string Northsouth { get; set; }
+        public string? Sex { get; set; }
+        public string? Northsouth { get; set; }
         public string Depth { get; set; }
         public string Eastwest { get; set; }
-        public string Adultsubadult { get; set; }
-        public string Facebundles { get; set; }
+        public string? Adultsubadult { get; set; }
+        public string? Facebundles { get; set; }
         public string Southtohead { get; set; }
-        public string Preservation { get; set; }
+        public string? Preservation { get; set; }
         public string Fieldbookpage { get; set; }
         public string Squareeastwest { get; set; }
         public string Goods { get; set; }
-        public string Text { get; set; }
+        public string? Text { get; set; }
         public string Wrapping { get; set; }
         public string Haircolor { get; set; }
         public string Westtohead { get; set; }
@@ -34,15 +34,15 @@ namespace Intex2Final.Models
         public string Burialnumber { get; set; }
         public string Dataexpertinitials { get; set; }
         public string Westtofeet { get; set; }
-        public string Ageatdeath { get; set; }
+        public string? Ageatdeath { get; set; }
         public string Southtofeet { get; set; }
         public string Excavationrecorder { get; set; }
-        public string Photos { get; set; }
+        public string? Photos { get; set; }
         public string Hair { get; set; }
-        public string Burialmaterials { get; set; }
+        public string? Burialmaterials { get; set; }
         public DateTime? Dateofexcavation { get; set; }
         public string Fieldbookexcavationyear { get; set; }
-        public string Clusternumber { get; set; }
+        public string? Clusternumber { get; set; }
         public string Shaftnumber { get; set; }
     }
 }
--- Intex2Final/Startup.cs
+++ Intex2Final/Startup.cs
@@ -49,13 +49,13 @@ namespace Intex2Final
                 options.Password.RequiredUniqueChars = 5;
             });
 
-            services.AddAuthentication().AddGoogle(options =>
-            {
-                IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
+            //services.AddAuthentication().AddGoogle(options =>
+            //{
+            //    IConfigurationSection googleAuthNSection = Configuration.GetSection("Authentication:Google");
 
-                options.ClientId = googleAuthNSection["ClientId"];
-                options.ClientSecret = googleAuthNSection["ClientSecret"];
-            });
+            //    options.ClientId = googleAuthNSection["ClientId"];
+            //    options.ClientSecret = googleAuthNSection["ClientSecret"];
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
